14117e4f0e18cdb2dd490253f01a29491501bb72
Nahé <nahema.hutin@gmail.com>
2022-05-04 16:27:35 +0200

    essai

--- front/js/script.js
+++ front/js/script.js
@@ -1,22 +1,48 @@
+fillSection();
 
-const fetchAllProducts = async () => {
-    try {
-      const url = ("http://localhost:3000/api/products");
-      const response = await fetch(url);
-      const data = await response.json();
-  console.log(data);
-  
-      return data;
-    } catch (err) {
-      console.log ("Erreur :  + err");
-      console.log ("Veuillez démarrer le serveur");
-      return null;
-    }
-  };
-  const renderPage = async () => {
-    const data = await fetchAllProducts();
-    data.forEach(function (sofa) {
-        renderHomeProduct(sofa);
-        console.log(sofa);
-      });
-    }
+// Récupération des articles de l'API
+async function getArticles() {
+    var articlesCatch = await fetch("http://localhost:3000/api/products")
+    return await articlesCatch.json();
+}
+
+    // Répartition des données de l'API dans le DOM
+async function fillSection() {
+    var result = await getArticles ()
+    .then(function (resultatAPI){
+        const articles = resultatAPI;
+        console.table(articles);
+        for (let article in articles) {
+
+            // Insertion de l'élément "a"
+            let productLink = document.createElement("a");
+            document.querySelector(".items").appendChild(productLink);
+            productLink.href = `product.html?id=${resultatAPI[article]._id}`;
+
+            // Insertion de l'élément "article"
+            let productArticle = document.createElement("article");
+            productLink.appendChild(productArticle);
+
+            // Insertion de l'image
+            let productImg = document.createElement("img");
+            productArticle.appendChild(productImg);
+            productImg.src = resultatAPI[article].imageUrl;
+            productImg.alt = resultatAPI[article].altTxt;
+
+            // Insertion du titre "h3"
+            let productName = document.createElement("h3");
+            productArticle.appendChild(productName);
+            productName.classList.add("productName");
+            productName.innerHTML = resultatAPI[article].name;
+
+            // Insertion de la description "p"
+            let productDescription = document.createElement("p");
+            productArticle.appendChild(productDescription);
+            productDescription.classList.add("productName");
+            productDescription.innerHTML = resultatAPI[article].description;
+        }
+    })
+    .catch (function(error){
+        return error;
+    });
+}
